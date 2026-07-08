bd29cc0bea85d21c81edc1a681ac54765c1ef3c8
jrose07 <julian.rosenbaum@tu-dortmund.de>
2026-07-08 12:02:46 +0200

    T_C with delta_plotting verbessert und komme auf das gleiche ergebnis wie mit näherung

--- BA_Python/scripts/abhaengigkeiten_simul copy.py
+++ BA_Python/scripts/abhaengigkeiten_simul copy.py
@@ -24,18 +24,23 @@ def get_T_C(U, E_D, T_array, start):
                 delta = np.nan
             deltas.append(delta)
         deltas = np.asarray(deltas)
-        mask = np.isclose(deltas, 0.0, rtol=1e-4, atol=1e-8)
+        abs_deltas = np.abs(deltas)
+        min_abs_delta = np.nanmin(abs_deltas)
+        mask = np.isclose(abs_deltas, min_abs_delta, rtol=1e-10)
         if not np.any(mask):
-            return np.nan
-        return np.min(T_array[mask])
+            T_C = np.nan
+        T_C = np.min(T_array[mask])
+        return T_C
     return np.vectorize(T_C_scalar, otypes=[float])(U_b, E_D_b)
 
-U = np.linspace(70,110,100)
-E_D = np.linspace(0.05,0.07,100)
-T = np.linspace(1,100,100)
+from graphene import mev2t
+U = np.linspace(75,110,10)
+E_D = np.linspace(mev2t(150),mev2t(200),10)
+T = np.linspace(1,700,100)
 
-T_C = get_T_C(U,E_D, T, 1)
-
+T_C = get_T_C(U,E_D, T, 0.5)
+A = 0.184080
+U_C = 1/(E_D*A)
 
 conv = t / const.e * 1e3
 
@@ -43,10 +48,12 @@ levels = np.linspace(np.nanmin(T_C), np.nanmax(T_C), 100)
 T_C_masked = np.ma.masked_invalid(T_C)
 fig, ax = plt.subplots()
 colorbar = ax.contourf(U*conv*1e-3, E_D*conv, T_C_masked, levels=levels, cmap='inferno')
+ax.plot(U_C*conv*1e-3, E_D*conv, label=r"$U_C$")
 fig.colorbar(colorbar, ax=ax, label=r"$T_C \, / \, K$")
 ax.set(
     xlabel=r"$U \, / \, eV$",
     ylabel=r"$E_D \, / \, meV$"
 )
 ax.set_facecolor(color='black')
-fig.savefig("../plots/T_C_with_delta_1.pdf")
+ax.legend()
+fig.savefig("../plots/T_C_with_delta_2.pdf")
